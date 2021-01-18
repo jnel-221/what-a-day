8439bb18dd57cba65c7c8abe96eb46d0183f70c2
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-01-17 18:56:43 -0600

    updated workHours array, updated variables in createSchedule function, labels now display array values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var today = moment().format('LLL')
 var now = moment().format('h a');
-console.log(now);
+console.log(typeof now);
 
-var workHours = [6,7,8,9,10,11,12,1,2,3,4,5,6]
+var workHours = [6+" am",7+" am",8+" am",9+" am",10+" am",11+" am",12+" pm",1+" pm",2+" pm",3+" pm",4+" pm",5+" pm",6+" pm"]
 
 
 $(document).ready(function(){
@@ -14,8 +14,8 @@ function createSchedule(){
     for(var i = 0; i < workHours.length; i++){
     var inptGrp = $('<div>').addClass('input-group time-block');
     var inptGrpPrend = $('<div>').addClass('input-group-prepend');
-    var spanEL =$('<span>').addClass('input-group-text hour').text("hello world");
-    var agendaItem = $('<input>').attr({'data-time': 0, 'type': 'text'}).addClass('form-control description time-block');
+    var spanEL =$('<span>').addClass('input-group-text hour').text(workHours[i]);
+    var agendaItem = $('<input>').attr({'data-time': workHours[i], 'type': 'text'}).addClass('form-control description time-block');
     var inptGrpAppend = $('<div>').addClass("input-group-append");
     var saveBtn = $('<button>').addClass('saveBtn').html('Save');
  inptGrp.append(inptGrpPrend);
