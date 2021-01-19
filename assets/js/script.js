028957c5d9466fd8f9d071ea66c2b882118e4c1f
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-01-18 19:21:35 -0600

    added id to input fields in createSchedule function, resolved the issue with retrieveAppts function, schedule fields now display stored text

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,6 @@ var currentDate =  $('#currentDay');
 
 
 // Global DOM elements for creating schedule:
-var inptGrpEl = $('<div class="input-group time-block">');
-var inptGrpPrend = $('<div class="input-group-prepend">');
-var spanEl =$('<span class="input-group-text hour">'); 
-var inptEl = $('<input class="form-control description time-block event" type="text">'); 
-var inptGrpAppend = $('<div class="input-group-append">');
-var saveBtnEl = $('<button class="saveBtn">').html('Save');
-
 
 var workHours = [6+" am",7+" am",8+" am",9+" am",10+" am",11+" am",12+" pm",1+" pm",2+" pm",3+" pm",4+" pm",5+" pm",6+" pm"]
 var dataHours = [6,7,8,9,10,11,12,13,14,15,16,17,18]
@@ -20,11 +13,6 @@ var dataHours = [6,7,8,9,10,11,12,13,14,15,16,17,18]
 //document on ready 
 $(document).ready(function(){
    currentDate.text(today);
-  
-
-
-
-
 });
 
 
@@ -39,7 +27,7 @@ function createSchedule(arr){
     var saveBtnEl = $('<button class="saveBtn">').html('Save');
 
     var label = spanEl.text(workHours[i]);
-    var inptAttr = inptEl.attr('data-time', dataHours[i]);
+    var inptAttr = inptEl.attr({'id': dataHours[i],'data-time': dataHours[i]});
 
     var dataInt = parseInt(inptAttr.attr('data-time'));
    
@@ -56,16 +44,14 @@ function createSchedule(arr){
  
      localStorage.setItem(apptTime, appt);
    });
-   
-  
    assignColor(dataInt, inptAttr);
+   retrieveAppts(inptAttr);
   };
   
- };
-
+};
 
 function assignColor (dataInt, inptAttr) {
-    console.log(dataInt);
+
 if (dataInt < now){
     inptAttr.addClass('past')
 }else if(dataInt > now){
@@ -75,13 +61,16 @@ if (dataInt < now){
 }
 };
 
-// function retrieveAppts(){
-//   for (var a = 6; a <= 18; a++){
-//   $(a).val(localStorage.getItem(a));  
+function retrieveAppts(inptAttr){
+    // var test = inptAttr.val(localStorage.getItem(6));  
+    // console.log(test);
+  for (var j = 6; j <= 18; j++){
+      console.log(j);
+      $('#'+j).val(localStorage.getItem(j));
 
-// }
-// };
-// retrieveAppts();
+    };
+};
+
 createSchedule(workHours,dataHours);
 
 
