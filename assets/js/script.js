feaed16855ee3a7a657912d4dfd7f783cb6071b3
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-01-18 19:42:11 -0600

    updated stylesheet, cleaned up notes and spacing on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ console.log(now);
 var currentDate =  $('#currentDay');
 
 
-// Global DOM elements for creating schedule:
+//Arrays for creating schedule:
 
 var workHours = [6+" am",7+" am",8+" am",9+" am",10+" am",11+" am",12+" pm",1+" pm",2+" pm",3+" pm",4+" pm",5+" pm",6+" pm"]
 var dataHours = [6,7,8,9,10,11,12,13,14,15,16,17,18]
@@ -22,7 +22,7 @@ function createSchedule(arr){
     var inptGrpEl = $('<div class="input-group time-block">');
     var inptGrpPrend = $('<div class="input-group-prepend">');
     var spanEl =$('<span class="input-group-text hour">'); 
-    var inptEl = $('<input class="form-control description time-block event" type="text">'); 
+    var inptEl = $('<input class="form-control description time-block event row" type="text">'); 
     var inptGrpAppend = $('<div class="input-group-append">');
     var saveBtnEl = $('<button class="saveBtn">').html('Save');
 
@@ -45,7 +45,7 @@ function createSchedule(arr){
      localStorage.setItem(apptTime, appt);
    });
    assignColor(dataInt, inptAttr);
-   retrieveAppts(inptAttr);
+   retrieveAppts();
   };
   
 };
@@ -61,13 +61,10 @@ if (dataInt < now){
 }
 };
 
-function retrieveAppts(inptAttr){
-    // var test = inptAttr.val(localStorage.getItem(6));  
-    // console.log(test);
+function retrieveAppts(){
   for (var j = 6; j <= 18; j++){
       console.log(j);
       $('#'+j).val(localStorage.getItem(j));
-
     };
 };
 
